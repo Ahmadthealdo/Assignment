66e4543c92d2324b5eef69be5740ce293b610043
Ahmad Abdullah <ahmadabdullah.2003@outlook.com>
2025-02-11 10:50:02 +0500

    Update LexicalAnalyzer.java

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,9 +1,7 @@
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.nio.file.Paths;
+import java.util.*
 
 public class LexicalAnalyzer {
     public static void main(String[] args) throws IOException {
